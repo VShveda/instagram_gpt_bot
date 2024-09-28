7907bd8392bbd07ecd4bd55d5230ee13321febaf
Viktor Sveda <vinix.ru@gmail.com>
2024-09-28 19:30:57 +0300

    add message function to message_generator.py

---
+++ botAPI/message_generator.py
@@ -0,0 +1,30 @@
+import os
+
+from google import generativeai as gemini
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+gemini.configure(api_key=GEMINI_API_KEY)
+
+
+def message(bio, objects_detected):
+
+    prompt = f"""
+    Я збираюся написати перше дружнє повідомлення людині на основі її профілю в Instagram. Ось що я знаю про людину: 
+    - Ім'я: {bio['full_name']}
+    - Опис профілю: {bio['description']}
+    - Виявлені об'єкти на фото: {objects_detected}
+    
+    Згенеруй привітальне повідомлення, яке буде ввічливим, 
+    приємним і покаже інтерес до її хобі чи того, що є в описі або на фотографії. 
+    Повідомлення повинно бути коротким і підходити для першого знайомства.
+    """
+
+    model = gemini.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt)
+
+    text = response.text
+
+    return text
