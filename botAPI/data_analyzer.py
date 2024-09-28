44e3d3a3c1d1da7506e9a22b3d4240a66d00fbbb
Viktor Sveda <vinix.ru@gmail.com>
2024-09-28 19:29:57 +0300

    add data_analyzer.py function for img

---
+++ botAPI/data_analyzer.py
@@ -0,0 +1,27 @@
+import os
+
+import requests
+from PIL import Image
+from io import BytesIO
+import google.generativeai as gemini
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+gemini.configure(api_key=gemini_api_key)
+
+
+def download_image(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    return img
+
+
+def img_description(url):
+    prompt = "Describe this image: "
+    img = download_image(url)
+    model = gemini.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content([prompt, img])
+    print(response.text)
